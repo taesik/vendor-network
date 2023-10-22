db51e8771e571bcda06e68ae7a3e92a94d7fffc9
taesik mun <taesic12@knou.ac.kr>
2023-10-23 08:01:10 +0900

    Added terraform main and variables

---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "aws_region" {
+
+}
+
+variable "app_name" {
+  description = "Name of the app"
+  default = "vendor-network"
+}
+
+variable "public_subnets" {
+  default = ["10.10.100.0/24","10.10.101.0/24"]
+}
+
+variable "private_subnets" {
+  default = ["10.10.0.0/24","10.10.1.0/24"]
+}
+
+variable "availability_zones" {
+  default = ["ap_northeast_2"]
+}
