f3859d1167b09f60883a4744d341fae4bcdfbe42
taesik mun <taesic12@knou.ac.kr>
2023-10-23 19:40:18 +0900

    Added igw, ngw, eip

--- terraform/ngw.tf
+++ terraform/ngw.tf
@@ -1,5 +1,5 @@
 resource "aws_nat_gateway" "ngw1" {
-  allocation_id = aws_eip.eip_1
+  allocation_id = aws_eip.eip_1.id
   subnet_id = aws_subnet.public_1.id
 
   depends_on = [
@@ -11,7 +11,7 @@ resource "aws_nat_gateway" "ngw1" {
   }
 }
 resource "aws_nat_gateway" "ngw2" {
-  allocation_id = aws_eip.eip_2
+  allocation_id = aws_eip.eip_2.id
   subnet_id = aws_subnet.public_2.id
 
   depends_on = [
