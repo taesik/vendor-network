36719e8c2a35faf96d3481c690a4582d8485afb0
taesik mun <taesic12@knou.ac.kr>
2023-10-23 16:11:05 +0900

    Added subnets.tf

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -22,7 +22,7 @@ resource "aws_subnet" "public_2" {
 
 resource "aws_subnet" "private_1" {
   vpc_id = aws_vpc.vpc.id
-  cidr_block = var.public_subnets[0]
+  cidr_block = var.private_subnets[0]
   availability_zone = var.availability_zones[0]
   map_public_ip_on_launch = true
 
@@ -32,7 +32,7 @@ resource "aws_subnet" "private_1" {
 }
 resource "aws_subnet" "private_2" {
   vpc_id = aws_vpc.vpc.id
-  cidr_block = var.public_subnets[1]
+  cidr_block = var.private_subnets[1]
   availability_zone = var.availability_zones[1]
   map_public_ip_on_launch = true
 
