9b4d38b9b6d697d5df9dd9ef05835e077d31574b
taesik mun <taesic12@knou.ac.kr>
2023-10-23 21:32:08 +0900

    Added rt

--- terraform/route_table.tf
+++ terraform/route_table.tf
@@ -73,5 +73,5 @@ resource "aws_route" "private_2" {
 
 resource "aws_route_table_association" "private_2" {
   subnet_id = aws_subnet.private_2.id
-  route_table_id = aws_route_table.private_2
+  route_table_id = aws_route_table.private_2.id
 }
